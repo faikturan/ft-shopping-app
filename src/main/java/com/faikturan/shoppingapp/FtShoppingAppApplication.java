6434301f80ace634ac130c0f7acaeb02cc1c8782
TURKCELL\faik.turan <faik.turan@consultant.turkcell.com.tr>
2021-10-26 12:27:07 +0300

    Ecommerce with Spring Boot and Vue.js initialized

---
+++ src/main/java/com/faikturan/shoppingapp/FtShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package com.faikturan.shoppingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtShoppingAppApplication.class, args);
+	}
+
+}
